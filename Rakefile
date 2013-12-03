195fa13572f494cae9496e053636ddb40715c6ed
Joe Yates <joe.g.yates@gmail.com>
2013-12-03 13:54:04 +0100

    User gtest

--- Rakefile
+++ Rakefile
@@ -1,8 +1,9 @@
 require 'rake/builder'
 
 Rake::Builder.new do | builder |
+  builder.logger.level         = ::Logger::DEBUG
   builder.target               = 'cstr-null-termination-in-cpp11'
   builder.architecture         = 'x86_64'
-  builder.compilation_options  = ['-std=c++0x', '-DDEBUG']
-  builder.logger.level         = ::Logger::DEBUG
+  builder.compilation_options  = ['-std=c++0x', '-DDEBUG', '-pthread']
+  builder.library_dependencies = ['gtest', 'pthread']
 end
--- cstr-null-termination-in-cpp11.cpp
+++ cstr-null-termination-in-cpp11.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include <string>
 #include <string.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+TEST(CStringNullTermination, BuildFromChars) {
   string buildFromChars;
   buildFromChars.push_back('c');
-  cout << "built from characters: " << strlen(buildFromChars.c_str()) << endl;
 
-  string fromCharPointer = "Hello World";
-  cout << "from char *: " << strlen(fromCharPointer.c_str()) << endl;
-
-  return 0;
+  ASSERT_EQ(1, strlen(buildFromChars.c_str()));
 }
 
+TEST(CStringNullTermination, FromCharPointer) {
+  string fromCharPointer = "Hello World";
+  ASSERT_EQ(11, strlen(fromCharPointer.c_str()));
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int result = -1;
+  return RUN_ALL_TESTS();
+}
