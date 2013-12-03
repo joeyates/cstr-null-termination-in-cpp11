c9fc9ec5aab1f55da72ad282137828e4946f37c3
Joe Yates <joe.g.yates@gmail.com>
2013-12-03 10:37:43 +0100

    Build a string from characters

--- cstr-null-termination-in-cpp11.cpp
+++ cstr-null-termination-in-cpp11.cpp
@@ -2,9 +2,15 @@
 #include <string>
 #include <string.h>
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
-  std::string s = "Hello World";
-  std::cout << strlen(s.c_str()) << std::endl;
+  string buildFromChars;
+  buildFromChars.push_back('c');
+  cout << "built from characters: " << strlen(buildFromChars.c_str()) << endl;
+
+  string fromCharPointer = "Hello World";
+  cout << "from char *: " << strlen(fromCharPointer.c_str()) << endl;
 
   return 0;
 }
