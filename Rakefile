d2a5746d1caf4d13c09aa8914bd00d9bd42a486b
Joe Yates <joe.g.yates@gmail.com>
2013-12-03 14:27:49 +0100

    Add a test for sqlite's bind_parameters

--- Rakefile
+++ Rakefile
@@ -5,5 +5,5 @@ Rake::Builder.new do | builder |
   builder.target               = 'cstr-null-termination-in-cpp11'
   builder.architecture         = 'x86_64'
   builder.compilation_options  = ['-std=c++0x', '-DDEBUG', '-pthread']
-  builder.library_dependencies = ['gtest', 'pthread']
+  builder.library_dependencies = ['gtest', 'sqlite3', 'pthread']
 end
--- cstr-null-termination-in-cpp11.cpp
+++ cstr-null-termination-in-cpp11.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <sqlite3.h>
+#include <sstream>
 #include <string>
 #include <string.h>
 
@@ -14,6 +16,82 @@ TEST(CStringNullTermination, BuildFromChars) {
 TEST(CStringNullTermination, FromCharPointer) {
   string fromCharPointer = "Hello World";
   ASSERT_EQ(11, strlen(fromCharPointer.c_str()));
+}
+
+class SqliteQuery {
+  public:
+    SqliteQuery() {
+      database_file_ = "query_test.sqlite3";
+      query_ = "SELECT english from translations where italian = ?";
+      delete_database();
+      connect();
+      create_table();
+      insert_rows();
+      prepare();
+    };
+
+    ~SqliteQuery() {
+      disconnect();
+      delete_database();
+    }
+
+    void bind(const string &italian) {
+      sqlite3_bind_text(statement_, 1, italian.c_str(), italian.size(), 0);
+    }
+
+    string execute() {
+      sqlite3_step(statement_);
+      const char * value = (const char *) sqlite3_column_text(statement_, 0);
+      string s = value;
+      return s;
+    }
+
+  private:
+    void delete_database() {
+      string remove_database = "rm -f " + database_file_;
+      system(remove_database.c_str());
+    };
+
+    void connect() {
+      sqlite3_open(database_file_.c_str(), &db_);
+    }
+
+    void create_table() {
+      string create_table = "CREATE TABLE translations (italian TEXT, english TEXT);";
+      sqlite3_stmt * statement;
+      sqlite3_prepare_v2(db_, create_table.c_str(), create_table.size(), &statement, 0);
+      sqlite3_step(statement);
+      sqlite3_finalize(statement);
+    }
+
+    void insert_rows() {
+      string insert = "INSERT INTO translations VALUES (\"ciao\", \"hello\");";
+      sqlite3_stmt * statement;
+      sqlite3_prepare_v2(db_, insert.c_str(), insert.size(), &statement, 0);
+      sqlite3_step(statement);
+      sqlite3_finalize(statement);
+    }
+
+    void prepare() {
+      sqlite3_prepare_v2(db_, query_.c_str(), query_.size(), &statement_, 0);
+    }
+
+    void disconnect() {
+      sqlite3_close(db_);
+    }
+
+    string           database_file_;
+    sqlite3 *        db_;
+    string           query_;
+    sqlite3_stmt *   statement_;
+};
+
+TEST(SqliteQuery, BindAndExecute) {
+  SqliteQuery qry;
+  qry.bind("ciao");
+  string result = qry.execute();
+
+  ASSERT_STREQ("hello", result.c_str());
 }
 
 int main(int argc, char *argv[]) {
